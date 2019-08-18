6ace8bb10eeb643bfa44c73346eebfe0e76c93b4
Sukaato <sukaato.dev@gmail.com>
2019-08-18 22:45:56 +0200

    Some change + rename variable

--- src/js/redirection.js
+++ src/js/redirection.js
@@ -1,9 +1,10 @@
 /* global location */
 /**
  * @name Redirection
+ * @class
  * @desc A Redirection object can be instanciated with an array of strings as first parameter or
- * @desc with languages as parameters (spread argument)
- * @desc First language will be selected as default language
+ * with languages as parameters (spread argument)
+ * First language will be selected as default language
  */
 class Redirection { // eslint-disable-line no-unused-vars
   /**
@@ -13,21 +14,29 @@ class Redirection { // eslint-disable-line no-unused-vars
    * or
    * new Redirection('fr-FR', 'en-US')
    */
-  constructor () {
-    this.pages = arguments.length === 1 && Array.isArray(arguments[0]) ? arguments[0] : Array.from(arguments)
-    if (!this.pages.length) {
-      throw new Error('RedirectionError: There should be at least one language in the constructor call')
-    }
-    if (!this.pages.every(lang => /^([a-z]{2}-[A-Z]{2})$/.test(lang))) {
-      throw new Error('RedirectionError: All languages should have the following format: aa-AA with aa, the language and AA, the region')
-    }
-    this.lang2page = {}
-    this.pages.forEach(page => {
-      const lang = page.split('-')[0]
-      if (!this.lang2page[lang]) {
-        this.lang2page[lang] = page
+  constructor() {
+    try {
+      const regex = /^([a-z]{2}-[A-Z]{2})$/;
+      this.availableLangs = arguments.length === 1 && Array.isArray(arguments[0]) ? arguments[0] : Array.from(arguments)
+      if (!this.availableLangs.length) {
+        throw new Error('RedirectionError: There should be at least one language in the constructor call')
       }
-    })
+
+      if (!this.availableLangs.every(lang => regex.test(lang))) {
+        throw new Error('RedirectionError: All languages should have the following format: aa-AA with aa, the language and AA, the region')
+      }
+
+      this.langsList = {}
+      this.availableLangs.forEach(page => {
+        const lang = page.split('-')[0]
+        if (!this.langsList[lang]) {
+          this.langsList[lang] = page
+        }
+      })
+      
+    } catch(err) {
+      console.error(err);
+    }
   }
 
   /**
@@ -39,11 +48,11 @@ class Redirection { // eslint-disable-line no-unused-vars
    * or
    * redirection.go('index.html', 2) // Redirecting to /lang/index.html after 2 seconds
    */
-  go (url, cooldown) {
-    const userlangs = navigator.languages || navigator.language || navigator.userLanguage
-    for (const lang of Array.isArray(userlangs) ? userlangs : [ userlangs ]) {
-      if (this.lang2page[lang]) return this._redirect(this.lang2page[lang], url, cooldown) // 'aa'-style language -> faster so checked first
-      if (this.pages.includes(lang)) return this._redirect(lang, url, cooldown) // 'aa-AA'-style language -> slower
+   go(url, cooldown) {
+    const userlangs = navigator.languages || navigator.language
+    for (const lang of Array.isArray(userlangs) ? userlangs : [userlangs]) {
+      if (this.langsList[lang]) return this._redirect(this.langsList[lang], url, cooldown) // 'aa'-style language -> faster so checked first
+      if (this.availableLangs.includes(lang)) return this._redirect(lang, url, cooldown) // 'aa-AA'-style language -> slower
     }
     return this._redirect(this.pages[0], url, cooldown) // default
   }
@@ -54,7 +63,7 @@ class Redirection { // eslint-disable-line no-unused-vars
    * @param  {String} [url]      Optional sub URL
    * @param  {Number} [cooldown] Optional timer cooldown before redirection (in seconds)
    */
-  _redirect (lang, url, cooldown) {
+  _redirect(lang, url, cooldown) {
     setTimeout(() => {
       location.href = `${lang}/${url || ''}`
     }, (cooldown || 0) * 1000)
